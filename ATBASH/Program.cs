c2f0b5404af2df29e3d67606f6e5bce279cbcb77
Yaakov <y0583275590@gmail.com>
2025-05-15 16:47:52 +0300

    הוספתי את שאר הפונקציות וערכתי את הקוד שיהיה מסודר

--- ATBASH/Program.cs
+++ ATBASH/Program.cs
@@ -10,15 +10,21 @@ namespace Atbash_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(disc("abckjgl lfl90"));
-            Console.WriteLine();
+            string SecretMeassege = "Lfi ulixvh ziv kivkzirmt uli z nzqli zggzxp lm gsv Arlmrhg vmvnb.\n" +
+                "\nGsv ilxpvg fmrgh ziv ivzwb zmw dzrgrmt uli gsv hrtmzo.\nYlnyh szev yvvm kozxvw mvzi pvb olxzgrlmh." +
+                "\nMfpsyz urtsgvih ziv hgzmwrmt yb uli tilfmw rmurogizgrlm." +
+                "\nGsv zggzxp droo yv hfwwvm zmw hgilmt -- gsvb dlm’g hvv rg xlnrmt." +
+                "\nDv nfhg hgzb srwwvm zmw pvvk gsv kozm hvxivg fmgro gsv ozhg nlnvmg." +
+                "\nErxglib rh mvzi. Hgzb ivzwb.";
+            Printmeassge(DangerousWordes(SecretMeassege), disc(SecretMeassege));
+            Console.ReadKey();
         }
 
 
         static string disc(string str_input)
         {
             string output = "";
-            str_input.ToLower();
+            str_input = str_input.ToLower();
             foreach (char item in str_input)
             {
                 if (char.IsLetter(item))
@@ -36,6 +42,46 @@ namespace Atbash_Project
 
             }
             return output;
+        }
+
+        static int DangerousWordes(string message)
+        {
+            message = disc(message);
+            List<String> MessageList = message.Split().ToList();
+            List<string> Dangerous = new List<string>() { "bomb", "nukhba", "fighter", "rocket", "secret", "bombs", "fighters", "rocketes" };
+            int points = 0;
+
+            foreach (string str in MessageList)
+            {
+                if (Dangerous.Contains(str))
+                {
+                    points++;
+                }
+            }
+
+
+            return points;
+        }
+
+        static void Printmeassge(int points, string meassege)
+        {
+
+            if (points >= 1 && points <= 5)
+            {
+                meassege += "\n WARNING" + " " + points;
+            }
+            else if (points >= 6 && points <= 10)
+            {
+                meassege += "\n DANGER" + " " + points;
+            }
+            else if (points >= 11 && points <= 15)
+            {
+                meassege += "\n ULTRA ALERT" + " " + points;
+            }
+            Console.WriteLine(meassege);
+
+
+
         }
     }
 }
